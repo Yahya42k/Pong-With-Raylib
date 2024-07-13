25ce3e7ba7160fa0c931de8a4be22a0360a9bd06
yahya42k <yahya.abdurrahim@gmail.com>
2024-07-13 01:06:50 -0400

    drawing added

--- main.cpp
+++ main.cpp
@@ -3,23 +3,51 @@
 
 using namespace std;
 
+class Ball{
+    public:
+    float x,y;
+    int speed_x,speed_y;
+    int radius;
+    void Draw(){
+DrawCircle(x,y,radius,WHITE);
+    }
+
+    void Update(){
+        x+=speed_x;
+        y+=speed_y;
+    }
+
+};
+
+Ball ball;
+
 int main () {
 
    
 
     cout << "Starting th game" << endl;
-
     const int screen_width=1280;
     const int screen_height=800;
-
     InitWindow(screen_width,screen_height,"My Pong Game!");
-SetTargetFPS(60);
+    SetTargetFPS(60);
+
+ball.radius=20;
+ball.x=screen_width/2;
+ball.y=screen_height/2;
+ball.speed_x=7;
+ball.speed_y=7;
+
 
 while(WindowShouldClose()==false){
+
 BeginDrawing();
 
+//updating()
+ball.Update();
+
 //drawing
-DrawCircle(screen_width/2,screen_height/2,20,WHITE);
+ClearBackground(BLACK);
+ball.Draw();
 DrawRectangle(10,screen_height/2-60,25,120,WHITE);
 DrawRectangle(screen_width-35,screen_height/2-60,25,120,WHITE);
 DrawLine(screen_width/2,0,screen_width/2,screen_height,WHITE);
